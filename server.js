91af70362c017490815df7560d056dd384aee1f3
Daniel Salib <dsalib@sas.upenn.edu>
2018-02-08 00:17:02 -0500

    finished react-tweet-stream
    currently following Elon Musk, UPenn, and Xbox

---
+++ server.js
@@ -0,0 +1,51 @@
+// Require our dependencies
+require('dotenv').config();
+var express = require('express'),
+  exphbs = require('express-handlebars'),
+  http = require('http'),
+  mongoose = require('mongoose'),
+  twitter = require('twitter'),
+  routes = require('./routes'),
+  config = require('./config'),
+  streamHandler = require('./utils/streamHandler');
+
+// Create an express instance and set a port variable
+var app = express();
+var port = process.env.PORT || 8080;
+
+// Set handlebars as the templating engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Disable etag headers on responses
+app.disable('etag');
+
+// Connect to our mongo database
+mongoose.connect('mongodb://'+process.env.MONGO_DB_CRED+'@ds129018.mlab.com:29018/twitter-stream-react',{
+useMongoClient: true,
+});;
+
+// Create a new ntwitter instance
+var twit = new twitter(config.twitter);
+
+// Index Route
+app.get('/', routes.index);
+
+// Page Route
+app.get('/page/:page/:skip', routes.page);
+
+// Set /public as our static content dir
+app.use("/", express.static(__dirname + "/public/"));
+
+// Fire this bitch up (start our server)
+var server = http.createServer(app).listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
+
+// Initialize socket.io
+var io = require('socket.io').listen(server);
+
+// Set a stream listener for tweets matching tracking keywords
+twit.stream('statuses/filter',{track: 'Elon Musk, UPenn, Xbox'}, function(stream){
+  streamHandler(stream,io);
+});
